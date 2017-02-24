563cf4a820808fcadb44736ef7e58b8ebc89a934
lakshmipriya04 <priyaindia95@ex.com>
2017-02-24 18:57:34 +0530

    changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
+/* Your code to build the resume will go here.
  */
+var name="lakshmi Priya";
+var name1=HTMLheaderName.replace("%data%", name);
+$("#header").append(name1);
+var job="Software Developer";
+var job1=HTMLheaderRole.replace("%data%", job);
+$("#header").append(job1);
+var bio = {
+    "name" : "lp",
+    "role" : "software developer",
+    "contacts": {
+        "HTMLmobile":"8870088700",
+        "email": "lp@gmail.com",
+    }
+};
+$("#main").append(bio.name);
+$("#main").append(bio["role"]);
+$("#main").append(bio["HTMLmobile"]);
